4d1a8e0d1b3d3957e5b6b80c58c5f8ab765b8568
TN0501 <sinru_hong@tc.emo-5.com.tw>
2026-01-05 13:23:51 +0800

    123

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -28,7 +28,7 @@ namespace test_B.Controllers
         {
             var data = _service.GetFakeProducts();
             return Ok(data);
-        } 
+        } 123
 
         [HttpGet("complex")]
         public IActionResult GetComplexData()
